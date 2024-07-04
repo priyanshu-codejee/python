e9a784cbdb94d81a7e37750a9b6eedfb9e6b51ab
priyanshu-codejee <priyanshu.codejee@gmail.com>
2024-07-05 03:48:55 +0530

    changes in string program

--- DataType/mystring.py
+++ DataType/mystring.py
@@ -2,3 +2,9 @@ fname = "Priyanshu"
 lname = "Kumar"
 full_name = fname + " " + lname
 print(full_name,type(full_name))
+
+my_first_name="Sandeep";
+my_second_name="Kumar";
+my_full_name=my_first_name+" "+my_second_name
+print(my_full_name);
+

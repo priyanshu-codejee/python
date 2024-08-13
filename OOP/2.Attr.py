1a0e0fb496bb5ad2a761947808796d61b08b94ca
unknown <priyanshu.codejee@gmail.com>
2024-08-14 01:52:02 +0530

    OOP Basics

---
+++ OOP/2.Attr.py
@@ -0,0 +1,21 @@
+class Dog():
+
+    def __init__(self,breed,name,spots):  
+        ''' init is a constructor for a class, 
+        Self creates an instance of the class and helps to refer to itself.
+        Slef keyword is automatically defind while creating a class in other languages but in python we have to explicitly define it.
+        '''
+
+        # Atrribute            
+        # We take in the arguement
+        # we assign it using self.attribute_name
+        self.breed = breed
+        self.name = name
+
+        #Expect T/F
+        self.spots = spots
+
+my_dog = Dog(breed = 'Lab', name = 'sammy', spots = False)
+print(type(my_dog.breed))
+print(type(my_dog.name))
+print(type(my_dog.spots))
